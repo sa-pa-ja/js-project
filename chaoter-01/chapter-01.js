b56af62dd5596b6bd7f8785ac536ffdb23ed3a29
saturnino <161881@unsaac.edu.pe>
2025-05-06 07:01:55 -0500

    If will be replaced by CRLF the next time Git touches it

--- chaoter-01/chapter-01.js
+++ chaoter-01/chapter-01.js
@@ -2,5 +2,16 @@
 // console.log(pi);
 
 const add = (a, b) => a + b;
-console.log(add(20,50));
+console.log(add(20, 50));
 
+const sum = (a, b) => {
+  return a + b;
+};
+
+if (sum(1, 3) === 4) {
+  console.log("Print to stdout with newline");
+} else {
+  console.log(
+    "Multiple arguments can be passed, with the first used as the primary"
+  );
+}
